63cf7e752002b70bd9a200d9a8b0fb2ad562ea3d
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-06 11:16:38 +0200

    Ajout de bouton pour selectionner le nombre de joueurs (non fonctionnel)

--- BlackJack/src/blackjack/EcranAcceuil.java
+++ BlackJack/src/blackjack/EcranAcceuil.java
@@ -17,35 +17,44 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
  
 public class EcranAcceuil extends JFrame {
-	 /**
-     * Ajout de l'image
-     */
-    String imgUrl="img/blackjack.jpg";
-    ImageIcon icone = new ImageIcon(imgUrl);
+	// Image
+	String imgUrl="img/blackjack.jpg";
+	ImageIcon icone = new ImageIcon(imgUrl);
+
+	// Bouton
+	JButton bouton2 = new JButton("2 Joueurs.");
+	JButton bouton3 = new JButton("3 Joueurs.");
+	JButton bouton4 = new JButton("4 Joueurs.");
+	JButton bouton5 = new JButton("5 Joueurs.");
 	
-  private JPanel container = new JPanel();
- 
-  private JLabel label = new JLabel(icone, JLabel.CENTER);
+	
+	private JPanel container = new JPanel();
+	private JLabel label = new JLabel(icone, JLabel.CENTER);
 
-  public EcranAcceuil(){
-    this.setTitle("Animation");
-    this.setSize(1080, 1920);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-    container.setBackground(Color.white);
-    container.setLayout(new BorderLayout());
-    
-    
-  
+	public EcranAcceuil(){
+		this.setTitle("BlackJack");
+		this.setSize(1080, 1920);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		container.setBackground(Color.white);
+		container.setLayout(new BorderLayout());
 
-    JPanel top = new JPanel();
-    top.add(label);
-
-    container.add(top, BorderLayout.CENTER);
-    this.setContentPane(container);
-    this.setVisible(true);            
-  }
+		JPanel top = new JPanel();
+		top.add(label);
+		
+		JPanel bottom = new JPanel();
+		bottom.add(bouton2);
+		bottom.add(bouton3);
+		bottom.add(bouton4);
+		bottom.add(bouton5);
+		
+		container.add(top, BorderLayout.CENTER);
+		container.add(bottom, BorderLayout.NORTH);
+		this.setContentPane(container);
+		this.setVisible(true);            
+	}
 }
