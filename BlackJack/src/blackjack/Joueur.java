2cd18beaeda377bee862b06528fbbac8f5699fd2
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-06 09:56:20 +0200

    ajout de la classe croupier et setup classe Joueur

--- BlackJack/src/blackjack/Joueur.java
+++ BlackJack/src/blackjack/Joueur.java
@@ -8,5 +8,25 @@ package blackjack;
  *
  */
 public class Joueur {
-
+	
+	// Cartes en main
+	private Carte[] cartesMain;
+	
+	// Total des points
+	private int point;
+	
+	// Boolean pour savoir si le joueur est éliminé
+	private boolean estElimine;
+	
+	/**
+	 * Constructeur
+	 * @param carteMain Les cartes que le joueurs à en main.
+	 * @param point Le total des point obtenu par les cartes.
+	 */
+	public Joueur(Carte[] cartesMain, int point)
+	{
+		this.cartesMain = cartesMain;
+		this.point = point;
+		this.estElimine = false;
+	}
 }
