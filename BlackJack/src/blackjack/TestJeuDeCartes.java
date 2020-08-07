08b874b93fec5adc8139003b110415ceb184fcf5
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-07 16:16:39 +0200

    Revert "Revert "Revert "Merge branch 'Simon' of https://github.com/MagnesSimon/BlackJack into Simon"""
    This reverts commit 1f8f2523a8a76060d9a490aaeb9b1f74cc0492e9.

--- BlackJack/src/blackjack/TestJeuDeCartes.java
+++ BlackJack/src/blackjack/TestJeuDeCartes.java
@@ -9,11 +9,7 @@ package blackjack;
  */
 
 import java.awt.BorderLayout;
-import java.awt.List;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -38,12 +34,8 @@ public class TestJeuDeCartes extends JFrame {
 	StringBuilder sortie = new StringBuilder();
 	JTextArea zoneSortie = new JTextArea();
 	
-	// Création de la liste de joueurs
-	LinkedList listeJoueurs = new LinkedList();
-	
-
-	
-	
+	// Creation du paquet
+	Paquet paquet = new Paquet();
 	/**
 	 * Construction de l'application
 	 */
@@ -51,17 +43,13 @@ public class TestJeuDeCartes extends JFrame {
 	{
 		// Appel du constructeur de la classe JFrame.
 		super("JeuDeCartes");
-	}
-
-	/**
-	 * Création automatique de joueurs
-	 */
-	public void startGame(int nb)
-	{
 		
 		// Ajout des composants au container
 		zoneSortie.setEditable(false);
 		getContentPane().add(new JScrollPane(zoneSortie), BorderLayout.CENTER);
+				
+		// Création du croupier
+		Croupier croupier = new Croupier(0, false);
 			
 		// Met à jour la zone de sortie
 		zoneSortie.setText(sortie.toString());
@@ -71,14 +59,20 @@ public class TestJeuDeCartes extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	}
 
-		/*
+	/**
+	 * Création automatique de joueurs
+	 */
+	public void choisirNbJoueurs(int nb)
+	{
 		setNbJoueurs(nb);	
 		sortie.append("Il y a " + nb + " joueurs! \n");
 		for (int i = 0; i < nb; i++ )
 		{
-		
+			Joueur joueur = new Joueur(i, 0,false);
+			joueur.setID(i);
+			
 			joueur.setMain(paquet.piocher(2));
 			sortie.append("\n Joueur: " + joueur.getIDAdd()  + " voici vos cartes \n");
 			for (int j = 0; j< joueur.getMain().length ; j++)
@@ -87,63 +81,10 @@ public class TestJeuDeCartes extends JFrame {
 			}
 		}
 		
-		croupier.setMain(paquet.piocher(2));
-		sortie.append("Le croupier a les cartes suivantes : ");
-		for (int j = 0; j< croupier.getMain().length ; j++)
-		{
-			sortie.append("- ").append( croupier.getMain()[j]).append("\n");
-		}
-		
-		// mise à jour de la zone de sortie
 		zoneSortie.setText(sortie.toString());
-		
-*/
-	}
-	/**
-	 * Creer le nombre de joueurs voulu ansi que le croupier
-	 * @param nb Le nombre de joueurs
-	 * @param ecran L'ecran d'acceuil sur lequel on clique pour savoir le nombre de joueurs
-	 */
-	public void creerJoueurs(int nb, EcranAcceuil ecran) {
-		// Création du croupier
-		Croupier croupier = new Croupier(0, false);
-		listeJoueurs.add(croupier);
-		
-		nb = ecran.getNbJoueurs();
-		for (int i = 0; i < ecran.getNbJoueurs(); i++)
-		{
-			Joueur joueur = new  Joueur(0, 0,false);
-			int j = i+1;
-			joueur.setID(j);
-			listeJoueurs.add(joueur);
-		}
 	}
 	
-	/**
-	 * Afficher les cartes du joueurs
-	 * @param id L'id du joueurs dont on veux afficher les cartes.
-	 */
-	public void afficher(int pos, Joueur joueur)
-	{
-		if ( pos == 0)
-		{
-			sortie.append("\n Le Croupier possède les cartes suivantes: \n");
-			for (int j = 0; j< getCroupier().getMain().length ; j++)
-			{
-			sortie.append("- ").append( getCroupier().getMain()[j]).append("\n");
-			}
-		}
-		else
-		{
-			sortie.append("\n Le joueur " + getIdJoueurs(pos) + " possede les cartes suivantes: \n");
-			for (int j = 0; j< getJoueurs(pos).getMain().length ; j++)
-			{
-				sortie.append("- ").append( getJoueurs(pos).getMain()[j]).append("\n");
-			}
-		}
-		// Met à jour la zone de sortie
-		zoneSortie.setText(sortie.toString());
-	}
+
 	
 	/**
 	 * @return the nbJoueurs
@@ -160,38 +101,6 @@ public class TestJeuDeCartes extends JFrame {
 	}
 	
 	/**
-	 * @param i Le joueurs voulu
-	 * @return le joueurs entré en parametre
-	 */
-	public Joueur getJoueurs(int i) {
-		return (Joueur) listeJoueurs.get(i);
-	}
-	/**
-	 * @return le croupier
-	 */
-	public Joueur getCroupier() {
-		return (Joueur) listeJoueurs.get(0);
-	}
-	/**
-	 * 
-	 */
-	public int getIdJoueurs(int i){
-		Joueur j ;
-		int id;
-		j = (Joueur) listeJoueurs.get(i);
-		id = j.getID();		
-		return id;
-	}
-
-
-	/**
-	 * @param listeJoueurs the listeJoueurs to set
-	 */
-	public void setListeJoueurs(LinkedList listeJoueurs) {
-		this.listeJoueurs = listeJoueurs;
-	}
-
-	/**
 	 * Début de l'exécution du test
 	 * @param args Les paramètre de la ligne de commande
 	 */
@@ -201,51 +110,6 @@ public class TestJeuDeCartes extends JFrame {
 		//new TestJeuDeCartes();
 		EcranAcceuil ecran = new EcranAcceuil();
 		
-		// Création de la partie
-		TestJeuDeCartes jeu = new TestJeuDeCartes();
-
-		// Creation du paquet
-		Paquet paquet = new Paquet();
-		
-
-		ecran.boutonStart.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				ecran.fermerFenetre();
-				jeu.creerJoueurs(ecran.getNbJoueurs(), ecran);
-				jeu.startGame(ecran.getNbJoueurs());
-				
-				jeu.getCroupier().setMain(paquet.piocher(2));
-				
-				for (int i = 0; i < ecran.getNbJoueurs(); i++)
-				{
-					jeu.getJoueurs(i).setMain(paquet.piocher(2));
-				}
-				
-				//jeu.afficher(1, jeu.getJoueurs(1));
-				for (int i =0 ; i < ecran.getNbJoueurs(); i++)
-				{
-					jeu.afficher(i, jeu.getJoueurs(i));
-				}
-
-			}
-		});
-		
-
-	/*	
-		for (int i = 0; i < ecran.getNbJoueurs(); i++)
-		{
-			Joueur joueur = new Joueur(i, 0,false);
-			joueur.setID(i);
-			joueur.setMain(paquet.piocher(2));
-			jeu.afficher(i, joueur);
-		}
-	*/	
-		
-		
-		// Fin du main
 	}
 
 }
