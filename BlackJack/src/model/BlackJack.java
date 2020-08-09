20d9551f6549b12269fb702204d76f944e14f55f
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-09 15:23:20 +0200

    Methode initialisation de la partie

--- BlackJack/src/model/BlackJack.java
+++ BlackJack/src/model/BlackJack.java
@@ -3,4 +3,26 @@ package model;
 
 public class BlackJack {
 	
+	Joueur croupier;
+	Joueur joueur;
+	Paquet paquet;
+	
+	public BlackJack ( Joueur crp, Joueur player)
+	{
+		croupier = crp;
+		joueur = player;
+		paquet = new Paquet();
+	}
+	
+	/**
+	 * Methode qui distribue deux carte au joueur et au croupier
+	 */
+	public void initialisationPartie() {
+		//Cartes du croupier
+		croupier.prendreCarte(paquet);
+		croupier.prendreCarte(paquet);
+		// Cartes du joueur
+		joueur.prendreCarte(paquet);
+		joueur.prendreCarte(paquet);
+	}
 }
