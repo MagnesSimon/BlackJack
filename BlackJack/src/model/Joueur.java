5b1a9c5d529a403075cebcf6677e0df4707508cc
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-09 14:41:46 +0200

    classe Joueur + constructeur

--- BlackJack/src/model/Joueur.java
+++ BlackJack/src/model/Joueur.java
@@ -1,1 +1,17 @@
 package model;
+
+
+import java.util.*;
+
+public class Joueur{
+	
+	protected ArrayList<Carte> main;
+	protected int valeurMain;
+	protected int point;
+	
+	public Joueur() {
+		main = new ArrayList<Carte>(12);
+		valeurMain = 0;
+		point = 0;
+	}
+}
