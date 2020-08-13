274d9c17755580c67227e90fd7cc87bddd14b050
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-13 18:31:58 +0200

    Ajout d'un écran d'acceuil

--- BlackJack/src/contrôleur/Partie.java
+++ BlackJack/src/contrôleur/Partie.java
@@ -1,5 +1,8 @@
 package contrôleur;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import model.*;
 import vue.*;
 
@@ -7,9 +10,21 @@ public class Partie {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BlackjackGUI jeu = new BlackjackGUI();
+		EcranAcceuil home = new EcranAcceuil();
 		
-		jeu.lancerJeu();
+		home.boutonStart.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				BlackjackGUI jeu = new BlackjackGUI();
+				jeu.lancerJeu();
+				home.fermerFenetre();
+			}
+		});
+		
+		//BlackjackGUI jeu = new BlackjackGUI();
+		//jeu.lancerJeu();
 	}
 
 }
