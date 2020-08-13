17a0dc089db8d3acff57e03a7c309a037f455490
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-08-13 19:35:46 +0200

    correction plusieurs lignes

--- BlackJack/src/vue/Regles.java
+++ BlackJack/src/vue/Regles.java
@@ -20,18 +20,18 @@ public class Regles extends JFrame {
 	
 	private JPanel container = new JPanel();
 	
-	private String str = "Règles du blackjack: \n "
+	private String str = "<html><b>Règles du blackjack: </b><br/> "
 			+ "Le joueur joue contre un croupier, le but est d'obtenir un score supérieur "
-			+ "à celui du croupier sans dépasser le score de 21.\n"
-			+ "Si le joueur obtiens un score de 21 en deux cartes, il fait un Blackjack et gagne automatiquement.\n"
-			+ "Le joueur reçois deux cartes pour démarrer la partie et choisit de piocher ou de se coucher. \n"
-			+ "Le chiffre sur la carte équivaut au nombre de point de celle-ci, les figure ont un score de 10.\n"
-			+ "L'as vaut 11, et si le score du joueur ou du croupier dépasse 21, l'as vaut 1.\n";
+			+ "à celui du croupier sans dépasser le score de 21.<br/>"
+			+ "Si le joueur obtiens un score de 21 en deux cartes, il fait un Blackjack et gagne automatiquement.<br/>"
+			+ "Le joueur reçois deux cartes pour démarrer la partie et choisit de piocher ou de se coucher. <br/>"
+			+ "Le chiffre sur la carte équivaut au nombre de point de celle-ci, les figure ont un score de 10.<br/>"
+			+ "L'as vaut 11, et si le score du joueur ou du croupier dépasse 21, l'as vaut 1.</html>";
 	
 	public Regles()
 	{
 		this.setTitle("Règles du blackjack");
-		this.setSize(800,600);
+		this.setSize(800,200);
 		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setBackground(Color.GRAY);
